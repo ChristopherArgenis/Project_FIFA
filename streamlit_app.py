439465ea2f845cd7aeab634295033e1e1d774a44
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-11 21:52:29 -0700

    Dataframe players 15

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+df_players_15 = pd.Dataframe("players_15.csv")
 
 st.title("Panel de Jugadores de la FIFA ⚽")
 st.sidebar.header("Navegación")
+st.dataframe(df_players_15)
