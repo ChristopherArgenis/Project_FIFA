04a96f195a3291f096c2cd50e05856de7a8f1cc7
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-11 22:12:38 -0700

    Delete Dataframe view and Sidebar options.
    Eliminar vista de Dataframe extenso y mostrar opciones de Sidebar.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-df_players_15 = pd.read_csv("players_15.csv")
-
 st.title("Panel de Jugadores de la FIFA ⚽")
 st.sidebar.header("Navegación")
-st.dataframe(df_players_15)
+opcion = st.sidebar.radio(
+    "Selecciona una año:",
+    ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
+)
