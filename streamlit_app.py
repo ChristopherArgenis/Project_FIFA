4a905cdcdc08c9f28328e40b6421803fe5549779
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-11 21:50:25 -0700

    Propuesta de Titulo y Sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Panel de Jugadores de la FIFA ⚽")
+st.sidebar.header("Navegación")
