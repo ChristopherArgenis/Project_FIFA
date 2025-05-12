9d7ac987268e06f4ca756bd8846be430601f2b7d
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-11 22:38:00 -0700

    Selectbox in Sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,5 @@ import pandas as pd
 
 st.title("Panel de Jugadores de la FIFA ⚽")
 st.sidebar.header("Navegación")
-opcion = st.sidebar.radio(
-    "Selecciona una año:",
-    ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
-)
+years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
+seleccion = st.sidebar.selectbox("Selecciona un año:", years)
