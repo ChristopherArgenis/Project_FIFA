cc322886f0e8c89ed5633235756327816758bc71
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-13 05:51:31 +0000

    Error en leer el csv corregido

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df_players_15 = pd.read_csv("df_players_2015.csv")
+df_players_15 = pd.read_csv("df_players_15.csv")
 
 st.title("Panel de Jugadores de la FIFA ⚽")
 st.sidebar.header("Navegación")
