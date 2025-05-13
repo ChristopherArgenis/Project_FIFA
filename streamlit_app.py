c33f1dddd08d87a507843b584d7fd23119dc21bf
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-13 06:59:00 +0000

    Testing search with autocomplete

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df_players_15 = pd.read_csv("df_players_15.csv")
+nombres_jugadores = df_players_15['Name'].tolist()
 
 st.title("Panel de Jugadores de la FIFA ⚽")
 st.sidebar.header("Navegación")
@@ -9,6 +10,8 @@ years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
 seleccion = st.sidebar.selectbox("Selecciona un año:", years)
 
 if seleccion == "2015":
+  search = st.text_input("Buscar jugadores por nombre:", autocomplete=nombres_jugadores)
+  st.write(search)
   col1, col2 = st.columns(2)
   col1.image(df_players_15["player_face_url"][0], width=300)
   col2.metric("Valor en Euros", value=df_players_15["value_eur"][0])
