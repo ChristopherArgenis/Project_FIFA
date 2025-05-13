411fb31263cc48b48eb05d03935ec145e420137a
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-13 05:52:07 +0000

    columns, not column

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
 seleccion = st.sidebar.selectbox("Selecciona un año:", years)
 
 if seleccion == "2015":
-  col1, col2 = st.column(2)
+  col1, col2 = st.columns(2)
   col1.image(df_players_15["player_face_url"][0])
   col2.metric("Valor en Euros", value=df_players_15["value_eur"][0])
