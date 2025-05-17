1df56f4ee179ffe9f5fe81b39f25f5594b17bc0b
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-16 23:19:59 -0700

    Fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,8 @@ def main_content(df, indice_actual):
         col2.image(player["club_logo_url"], width=75)
         st.metric("Valuación", value=int(player["value_eur"]))
         st.divider()
-        st.metric("Habilidad General", value=int(players["overall"]))
-        st.metric("Potencial", value=int(players["potential"]))
+        st.metric("Habilidad General", value=int(player["overall"]))
+        st.metric("Potencial", value=int(player["potential"]))
         st.button("Anterior Jugador", on_click=anterior_jugador)
         st.button("Siguiente Jugador", on_click=siguiente_jugador)
 
