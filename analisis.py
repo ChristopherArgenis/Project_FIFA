f3ba0dd4be8b667e532b42b2c07737f564082200
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-23 11:07:31 -0700

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -17,6 +17,20 @@ columns_required = ["sofifa_id","player_url", "short_name", "long_name", "overal
                     "defending", "physic", "skill_dribbling", "skill_curve", "skill_ball_control", "movement_agility", "movement_reactions", "power_shot_power",
                     "power_jumping", "player_face_url", "club_logo_url", "nation_logo_url"]
 
+new_columns_requerid = ["short_name", "long_name", "overall", "potential", "value_eur", "wage_eur", "age", "height_cm", "weight_kg",
+                    "club_name", "club_position", "club_jersey_number", "nationality_name", "preferred_foot", "pace", "shooting", "passing", "dribbling",
+                    "defending", "physic", "skill_dribbling", "skill_curve", "skill_ball_control", "movement_agility", "movement_reactions", "power_shot_power",
+                    "power_jumping", "player_face_url", "club_logo_url"]
+
+# Tablas
+Jugador = ["long_name", "short_name", "age", "height_cm", "weight_kg", "player_face_url", "club_jersey_number", "preferred_foot"]
+posicion = ["club_position"]
+Pais = ["nationality_name"]
+club = ["club_name", "club_logo_url"]
+Finanzas = ["value_eur", "wage_eur"]
+metricas = ["overall", "potential", "pace", "shooting", "passing", "dribbling", "defending", "physic"]
+habilidades = ["skill_dribbling", "skill_curve", "skill_ball_control", "movement_agility", "movement_reactions", "power_shot_power", "power_jumping"]
+
 # Formato a Guardar.
 # Solo cambiar la parte "df_players_<año>.csv".
 # --- players.to_csv("df_players_22.csv", index=False, columns=columns_required) ---
