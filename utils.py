7aee1e2ab980d51954bc7858b09bde64464774fc
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-25 13:45:37 -0700

    Icons

--- utils.py
+++ utils.py
@@ -123,7 +123,7 @@ def comparar_metricas(j1, j2):
         mostrar_jugador_comparador(j2)
 
     st.markdown("---")
-    st.markdown("### Métricas Generales")
+    st.markdown("### Métricas Generales 🧠")
 
     for metrica in metricas_generales:
         v1 = j1[metrica]
@@ -146,7 +146,7 @@ def comparar_metricas(j1, j2):
                 st.metric(label=metrica, value="No disponible")
 
     st.markdown("---")
-    st.markdown("### Habilidades Técnicas")
+    st.markdown("### Habilidades Técnicas 🎯")
 
     for metrica in metricas_habilidades:
         v1 = j1[metrica]
