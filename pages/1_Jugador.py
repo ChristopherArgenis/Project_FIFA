21857d3c8c2df966392b482a6e329fad54737d87
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-05-25 11:09:50 -0700

    Update 1_Jugador.py

--- pages/1_Jugador.py
+++ pages/1_Jugador.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("🎮 Jugador Individual")
+
+years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]
+year = st.selectbox("Selecciona un año:", years)
